51f3d8551e46a39b81f4361b1e3b768ea47b7326
dkdla93 <67941919+dkdla93@users.noreply.github.com>
2025-09-14 23:36:02 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import BookSearchApp from './components/BookSearchApp';
+import BookSearchApp from './components/BookSearchApp.js';
 import './App.css';
 
 function App() {
